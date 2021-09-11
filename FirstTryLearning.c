fd207bf876c62c655074cd0cad20e5ec141bdc76
Fortuide <forallgamee2@gmail.com>
2021-09-11 12:55:14 +0200

    Float IEEE

--- FirstTryLearning.c
+++ FirstTryLearning.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
+#include<stdint.h>
+  
+typedef union {
+    float f;
+    struct{
+        unsigned int mantissa : 23;
+        unsigned int exponent : 8;
+        unsigned int sign : 1;    
+    }raw;
+}convFloat;
 
 int main()
 {
-    printf("Fuck this shit\n");
-    printf("Fuck this shit Twice\n");
+    convFloat var;
+    uint32_t x;
+    var.f = -2.25;
+    x = var.raw.sign << 31;
+    x = x | (var.raw.exponent << 23);
+    x = x | (var.raw.mantissa);
+    printf("%X\n", x);
     return 0;
 }
-
